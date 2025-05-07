ac87ee4c7bea5763d06707cc79c0226fa1b2ef32
Raven <magbanuaraven88@gmail.com>
2025-05-07 14:34:58 +0800

    Servo not fix

--- recieverESP.ino
+++ recieverESP.ino
@@ -1,134 +1,146 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <ESP32Servo.h>
 
 #define enA 26
-#define in1 12
+#define in1 16
 #define in2 13
-#define in3 27
+#define in3 27 
 #define in4 14
 #define enB 25
 
-const int motorSpeed = 200;
+#define SERVO_PIN 18
 
-#define R_S 2
-#define L_S 4
+Servo myservo;
+
+const int motorSpeed = 255;
+
 
 typedef struct struct_message {
-  int joyY;        
-  int leftButton;  
+  int joyY;
+  int leftButton;
   int rightButton;
   int joyX;
   int forwardButton;
   int backwardButton;
+  int pickButton;
+  int releaseButton;
 } struct_message;
 
 struct_message incomingData;
 
-void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *incomingDataPtr, int len) {
-  memcpy(&incomingData, incomingDataPtr, sizeof(incomingData));
-
-  Serial.println("Data Received:");
-  Serial.print("Joy Y: "); Serial.println(incomingData.joyY);
-  Serial.print("Left Button: "); Serial.println(incomingData.leftButton);
-  Serial.print("Right Button: "); Serial.println(incomingData.rightButton);
-  Serial.print("Joy X: "); Serial.println(incomingData.joyX);
-  Serial.print("Forward Button: "); Serial.println(incomingData.forwardButton);
-  Serial.print("Backward Button: "); Serial.println(incomingData.backwardButton);
-  Serial.println("-----------------------------");
-
-  handleMovement();
-}
-
 void setup() {
   Serial.begin(115200);
-  pinMode(in1, OUTPUT);
+
+  pinMode(in1, OUTPUT); 
   pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
+  pinMode(in3, OUTPUT); 
   pinMode(in4, OUTPUT);
-  pinMode(enA, OUTPUT);
+  pinMode(enA, OUTPUT); 
   pinMode(enB, OUTPUT);
 
-  WiFi.mode(WIFI_STA);
+  // pinMode(R_S, INPUT);
+  // pinMode(L_S, INPUT);
 
+  myservo.attach(SERVO_PIN, 1000, 2000);  // wider range
+  myservo.write(0);
+
+
+
+  WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     Serial.println("ESP-NOW Init Failed");
     return;
   }
-
   esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop() {
- 
+
 }
 
-void handleMovement() {
-  int joyY = incomingData.joyY;
+void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
+  memcpy(&incomingData, data, sizeof(incomingData));
+  // lastReceivedTime = millis();
+  handleRemoteControl();
+}
+
+void handleRemoteControl() {
+  int x = incomingData.joyX;
+  int y = incomingData.joyY;
   int leftBtn = incomingData.leftButton;
   int rightBtn = incomingData.rightButton;
+  int fwdBtn = incomingData.forwardButton;
+  int backBtn = incomingData.backwardButton;
+  int pickBtn = incomingData.pickButton;
+  int releaseBtn = incomingData.releaseButton;
 
-  int joyX = incomingData.joyX;
-  int forwardBtn = incomingData.forwardButton;
-  int backwardBtn = incomingData.backwardButton;
+  if(pickBtn == 0){
+    myservo.write(180);
+    delay(10);
+  };
+  if(releaseBtn == 0){
+    myservo.write(0);
+    delay(10);
 
+  };
 
-  if (leftBtn == 0 || joyX > 3000) {
+  if (x > 3000 || leftBtn == 0) {
     turnLeft();
-  } else if (rightBtn == 0 || joyX < 1000) {
+  } else if (x < 1000 || rightBtn == 0) {
     turnRight();
+  } else if (y > 3000 || fwdBtn == 0) {
+    moveForward();
+  } else if (y < 1000 || backBtn == 0) {
+    moveBackward();
   } else {
-    // Joystick-based movement (forward/backward)
-    if (joyY > 3000 || forwardBtn == 0) {
-      moveForward(); 
-    } else if (joyY < 1000 || backwardBtn == 0) {
-      moveBackward(); 
-    } else {
-      stopMotors(); 
-    }
+    stopMotors();
   }
 }
 
 void moveForward() {
-  digitalWrite(in1, HIGH);
+  digitalWrite(in1, HIGH); 
   digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
+  digitalWrite(in3, LOW); 
   digitalWrite(in4, HIGH);
   analogWrite(enA, motorSpeed);
   analogWrite(enB, motorSpeed);
 }
 
 void moveBackward() {
-  digitalWrite(in1, LOW);
+  digitalWrite(in1, LOW); 
   digitalWrite(in2, HIGH);
-  digitalWrite(in3, HIGH);
+  digitalWrite(in3, HIGH); 
   digitalWrite(in4, LOW);
   analogWrite(enA, motorSpeed);
   analogWrite(enB, motorSpeed);
 }
 
 void turnLeft() {
-  digitalWrite(in1, LOW);
+  digitalWrite(in1, LOW); 
   digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
+  digitalWrite(in3, LOW); 
   digitalWrite(in4, HIGH);
   analogWrite(enA, 0);
   analogWrite(enB, motorSpeed);
 }
 
 void turnRight() {
-  digitalWrite(in1, HIGH);
+  digitalWrite(in1, HIGH); 
   digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
+  digitalWrite(in3, LOW); 
   digitalWrite(in4, LOW);
   analogWrite(enA, motorSpeed);
   analogWrite(enB, 0);
 }
 
 void stopMotors() {
-  digitalWrite(in1, LOW);
+  digitalWrite(in1, LOW); 
   digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
+  digitalWrite(in3, LOW); 
   digitalWrite(in4, LOW);
-  analogWrite(enA, 0);
+  analogWrite(enA, 0);  
   analogWrite(enB, 0);
 }
+
+
